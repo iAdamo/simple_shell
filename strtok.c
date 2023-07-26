110a81131936bc6f29e6e49a19e68a55e94569d0
Damilola Ifabiyi <damilolamary39@gmail.com>
2023-07-26 12:08:15 -0700

    function that extracts tokens of a string

---
+++ strtok.c
@@ -0,0 +1,65 @@
+#include "unixshell.h"
+/**
+ * check_delim - function that checks if a character matchs any character
+ * @c: character to check
+ * @str: string of delimiters
+ * Return: 1 on success, 0 on failure
+ */
+unsigned int check_delim(char c, const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (c == str[i])
+			return(1);
+		i++;
+	}
+	return (0);
+}
+/**
+ * _strtok - function that extracts tokens of a string
+ * @str: string
+ * @delim: delimiter
+ * Return: pointer to the next token or NULL
+ */
+char *_strtok(char *str, const char *delim)
+{
+	static char *tokens;
+	static char *new_token;
+	unsigned int i;
+
+	if (str != NULL)
+		new_token = str;
+	tokens = new_token;
+	if (tokens == NULL)
+		return (NULL);
+	for (i = 0; tokens[i] != '\0'; i++)
+	{
+		if (check_delim(tokens[i], delim) == 0)
+			break;
+	}
+	if (new_token[i] == '\0' || new_token[i] == '#')
+	{
+		new_token = NULL;
+		return (NULL);
+	}
+	tokens = new_token + i;
+	new_token = tokens;
+	for (i = 0; new_token[i] != '\0'; i++)
+	{
+		if (check_delim(new_token[i], delim) == 1)
+			break;
+	}
+	if (new_token[i] == '\0')
+		new_token = NULL;
+	else
+	{
+		new_token[i] = '\0';
+		new_token = new_token + i + 1;
+		if (*new_token == '\0')
+			new_token = NULL;
+	}
+	return (tokens);
+}
